6aacfafbd8a60b8f97114fe76db8773e5c63a2b9
yiliyavas <59742742+yiliyavas@users.noreply.github.com>
2022-12-09 21:30:21 -0500

    done !

---
+++ listing.java
@@ -0,0 +1,59 @@
+
+
+public class listing {
+	protected String listid;
+	protected String owner;
+	protected String nftid;
+	protected String startTime;
+	protected String endTime;
+	protected String price;
+
+	public String getListid() {
+		return listid;
+	}
+
+	public void setListid(String listid) {
+		this.listid = listid;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getNftid() {
+		return nftid;
+	}
+
+	public void setNftid(String nftid) {
+		this.nftid = nftid;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+};
